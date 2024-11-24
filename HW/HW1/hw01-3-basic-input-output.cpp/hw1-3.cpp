b420d575c51136bcf29b2128aa74cb3926a24134
Sittichai04785 <sittichai.manorat@gmail.com>
2024-11-24 13:56:45 +0700

    Add files via upload

---
+++ HW/HW1/hw01-3-basic-input-output.cpp/hw1-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    char first[100] ;
+    char last[100] ;
+    printf( "First Name: " ) ;
+    scanf( "%s" , first ) ;
+    printf( "Last Name: " ) ;
+    scanf( "%s" , last ) ;
+    printf( "%s %ss TC, RMUTL, Chiang Mai, Thailand" , first , last ) ;
+    return 0 ;
+}//end function
